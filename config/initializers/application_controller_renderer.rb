250dec08e803683c05479f4edadc34d084904f26
utkazu <hiroshimazeal@gmail.com>
2020-03-09 01:18:29 +0900

    [Finish] 5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '8a567c1079a36a5211b568ee1eec6a9dbdb54568f7966efb6465fe42b4413b65ae9b1e8b74cbea77cae95faa7fd8f72dd5ee975f766885bcbe300a927cdf65e2'
